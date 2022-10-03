806be5ec546c2f94263b3c5d09482b6cda23854b
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-03 21:58:46 +0300

    add teacher class

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require "./person.rb"
+
+class Teacher < Person
+    attr_accessor :specialization
+    def initialize(specialization)
+        @specialization = specialization
+        super(name, age, parent_permission: true)
+    end
+
+    def can_use_services?
+        true
+    end
+end
