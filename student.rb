a22cb367d482ef0a99b62e0290e971754b840e9e
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-03 21:58:35 +0300

    add student class

---
+++ student.rb
@@ -0,0 +1,13 @@
+require "./person.rb"
+
+class Student < Person
+    attr_accessor :classroom
+    def initialize(classroom)
+        @classroom = classroom
+        super(name, age, parent_permission: true)
+    end
+
+    def play_hooky
+        "¯\(ツ)/¯"
+    end
+end
